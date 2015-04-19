30dece6deea805ebb55d74bfe8c2bdc8c0b3d526
preveyj <preveyj@gmail.com>
2015-04-19 16:00:47 -0600

    fixed tabs and indents
    Fixed indents to use tabs only

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -8,22 +8,22 @@ import psycopg2
 # global connection object?
 
 def connect():
-    """Connect to the PostgreSQL database.  Returns a database connection."""
-    return psycopg2.connect("dbname=tournament")
+	"""Connect to the PostgreSQL database.  Returns a database connection."""
+	return psycopg2.connect("dbname=tournament")
 
 
 def deleteMatches():
-    """Remove all the match records from the database."""
-    conn = connect()
-    if conn != None:
-    	cur = conn.cursor()
-    	cur.execute("DELETE FROM tbl_matches")
-    	cur.close()
-    	conn.close()
+	"""Remove all the match records from the database."""
+	conn = connect()
+	if conn != None:
+		cur = conn.cursor()
+		cur.execute("DELETE FROM tbl_matches")
+		cur.close()
+		conn.close()
 
 
 def deletePlayers():
-    """Remove all the player records from the database."""
+	"""Remove all the player records from the database."""
 	conn = connect()
 	if conn != None:
 		cur = conn.cursor()
@@ -32,25 +32,29 @@ def deletePlayers():
 		conn.close()
 
 def countPlayers():
-    """Returns the number of players currently registered."""
-    conn = connect()
-    if conn != None:
-    	cur = conn.cursor()
-    	cur.execute("SELECT COUNT(*) FROM tbl_players")
-    	return cur.fetchone()[0]
-    	cur.close()
-    	conn.close()
+	"""Returns the number of players currently registered."""
+	playerCount = 0
+	conn = connect()
+	if conn != None:
+		cur = conn.cursor()
+		cur.execute("SELECT COUNT(*) FROM tbl_players")
+		playerCount = cur.fetchone()[0]
+		cur.close()
+		conn.close()
+	
+	
+	return playerCount
 
 
 def registerPlayer(name):
-    """Adds a player to the tournament database.
+	"""Adds a player to the tournament database.
   
-    The database assigns a unique serial id number for the player.  (This
-    should be handled by your SQL database schema, not in your Python code.)
+	The database assigns a unique serial id number for the player.  (This
+	should be handled by your SQL database schema, not in your Python code.)
   
-    Args:
-      name: the player's full name (need not be unique).
-    """
+	Args:
+	  name: the player's full name (need not be unique).
+	"""
 	conn = connect()
 	if conn != None:
 		cur = conn.cursor()
@@ -60,60 +64,73 @@ def registerPlayer(name):
 	
 
 def playerStandings():
-    """Returns a list of the players and their win records, sorted by wins.
+	"""Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+	The first entry in the list should be the player in first place, or a player
+	tied for first place if there is currently a tie.
 
-    Returns:
-      A list of tuples, each of which contains (id, name, wins, matches):
-        id: the player's unique id (assigned by the database)
-        name: the player's full name (as registered)
-        wins: the number of matches the player has won
-        matches: the number of matches the player has played
-    """
-    results = []
-    
-    conn = connect()
-    if conn != None:
-    	cur = conn.cursor()
+	Returns:
+	  A list of tuples, each of which contains (id, name, wins, matches):
+		id: the player's unique id (assigned by the database)
+		name: the player's full name (as registered)
+		wins: the number of matches the player has won
+		matches: the number of matches the player has played
+	"""
+	results = []
+	
+	conn = connect()
+	if conn != None:
+		cur = conn.cursor()
 		cur.execute("SELECT winner_id, name, wins, matchcount FROM vw_rankings")
 		for record in cur:
-			results.append({record.winner_id, record.name, record.wins, record.matchcount})
+			results.append({"id": record.winner_id, "name": record.name, "wins": record.wins, "matches": record.matchcount})
 		
 		cur.close()
 		conn.close()
+		
+	print results
+	return results
 
 def reportMatch(winner, loser):
-    """Records the outcome of a single match between two players.
+	"""Records the outcome of a single match between two players.
 
-    Args:
-      winner:  the id number of the player who won
-      loser:  the id number of the player who lost
-    """
-    
-    conn = connect()
-    if conn != None:
-    	cur = conn.cursor()
-    	cur.execute("INSERT INTO tbl_Matches (winner_id, loser_id, result) values (%s, %s, true);", {winner, loser})
-    	cur.close()
-    	conn.close()
+	Args:
+	  winner:  the id number of the player who won
+	  loser:  the id number of the player who lost
+	"""
+	
+	conn = connect()
+	if conn != None:
+		cur = conn.cursor()
+		cur.execute("INSERT INTO tbl_Matches (winner_id, loser_id, result) values (%s, %s, true);", {winner, loser})
+		cur.close()
+		conn.close()
  
  
 def swissPairings():
-    """Returns a list of pairs of players for the next round of a match.
+	"""Returns a list of pairs of players for the next round of a match.
   
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
+	Assuming that there are an even number of players registered, each player
+	appears exactly once in the pairings.  Each player is paired with another
+	player with an equal or nearly-equal win record, that is, a player adjacent
+	to him or her in the standings.
   
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
-
-
+	Returns:
+	  A list of tuples, each of which contains (id1, name1, id2, name2)
+		id1: the first player's unique id
+		name1: the first player's name
+		id2: the second player's unique id
+		name2: the second player's name
+	"""
+	
+	thePairings = []
+	rankings = playerStandings()
+	
+	x = 0
+	while x < len(rankings) :
+		thePairings.append({"id1": rankings[x].id, "name1" : rankings[x].name, "id2" : rankings[x + 1].id, "name2" : rankings[x + 1].name})
+		
+		x = x + 2
+		
+	print thePairings
+	return thePairings
